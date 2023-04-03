e187619069703b647e83562a1e99eed0dba4093e
unknown <pinpailin.watc@kmutt.ac.th>
2023-04-03 10:45:45 +0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿class Program {
+    static void Main(string[] args) {
+        int input1 = int.Parse(Console.ReadLine());
+        int input2 = int.Parse(Console.ReadLine());
+        int input3 = int.Parse(Console.ReadLine());
+        int x = Number(input1,input2,intput3);
+        Console.WriteLine(x);
+
+    }
+    static int Number(int num1, int num2, int num3){
+        printEnd();
+        
+    }
+    static void printEnd(){
+        Console.WriteLine(End);
+    }
+}
