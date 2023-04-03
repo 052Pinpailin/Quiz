b1b592c7d6eb9b980825a48aeddcc37b21fde8c9
unknown <pinpailin.watc@kmutt.ac.th>
2023-04-03 10:49:31 +0700

    first commit

--- Program.cs
+++ Program.cs
@@ -3,15 +3,36 @@
         int input1 = int.Parse(Console.ReadLine());
         int input2 = int.Parse(Console.ReadLine());
         int input3 = int.Parse(Console.ReadLine());
-        int x = Number(input1,input2,intput3);
+        int x = Number(input1,input2,input3);
         Console.WriteLine(x);
 
     }
+    static void Main(string[] args) {
+        string FindMax = int.(Console.ReadLine());
+        string FindMin = int.(Console.ReadLine());
+        string FindMean = int.(Console.ReadLine());
+        string y = Fee(FindMax,FindMin,FindMean);
+        Console.WriteLine(y);
+        
+        if ((Fee == "FindMax")) {
+            if()
+            return Number;
+        }else if ((Fee == "FindMin")){
+            if(Number )
+            return Number
+            
+        }else if ((Fee == "FindMean")){
+            if(Number )
+            return Number;
+        }
+        
+
+    }
+    }
     static int Number(int num1, int num2, int num3){
         printEnd();
-        
     }
     static void printEnd(){
         Console.WriteLine(End);
-    }
 }
+
